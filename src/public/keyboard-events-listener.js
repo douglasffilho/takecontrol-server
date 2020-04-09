32a3f9e6c6201ce195edd23a9dd773ff36c5d8c6
Douglas Fernandes <douglasf.filho@gmail.com>
2020-04-09 17:47:44 -0300

    add: diffrent way to dispatch events (without mouse)

--- src/public/keyboard-events-listener.js
+++ src/public/keyboard-events-listener.js
@@ -1,23 +1,80 @@
-var commands = {
-    40: 'DOWN',
-    38: 'UP',
-    37: 'LEFT',
-    39: 'RIGHT',
-    83: 'DOWN',
-    87: 'UP',
-    65: 'LEFT',
-    68: 'RIGHT',
-    32: 'JUMP',
+var staticCommands = {
+    40: 'SWIPE-DOWN',
+    38: 'SWIPE-UP',
+    37: 'SWIPE-LEFT',
+    39: 'SWIPE-RIGHT',
+    32: 'SHOT',
     71: 'GRANADE',
     88: 'AIM',
-    17: 'SQUAT'
+    17: 'SQUAT',
+    18: 'JUMP'
 };
+
+var motionCommands = {
+    83: {
+        hit: false,
+        startCommand: 'START-MOVING-DOWN',
+        stopCommand: 'STOP-MOVING-DOWN'
+    },
+    87: {
+        hit: false,
+        startCommand: 'START-MOVING-UP',
+        stopCommand: 'STOP-MOVING-UP'
+    },
+    65: {
+        hit: false,
+        startCommand: 'START-MOVING-LEFT',
+        stopCommand: 'STOP-MOVING-LEFT'
+    },
+    68: {
+        hit: false,
+        startCommand: 'START-MOVING-RIGHT',
+        stopCommand: 'STOP-MOVING-RIGHT'
+    }
+};
+
+function isStatic(event) {
+    return !!staticCommands[event.keyCode];
+}
+
+function isMotion(event) {
+    return !!motionCommands[event.keyCode];
+}
+
+function getMotionCommand(event) {
+    return motionCommands[event.keyCode];
+}
+
+function getStaticCommand(event) {
+    return staticCommands[event.keyCode];
+}
 
 (function() {
     document.addEventListener('keydown', function(event) {
-        var command = commands[event.keyCode];
-        var direction = command ? command : 'STAY' ;
+        event.preventDefault();
+        if (isMotion(event)) {
+            var command = getMotionCommand(event);
+            if (!command.hit) {
+                command.hit = true;
+                
+                wsConnection.send(command.startCommand);
+            }
+        } else if (isStatic(event)) {
+            var command = getStaticCommand(event);
 
-        wsConnection.send(direction);
-    });
+            wsConnection.send(command);
+        }
+    }, false);
+
+    document.addEventListener('keyup', function(event) {
+        event.preventDefault();
+        if (isMotion(event)) {
+            var command = getMotionCommand(event);
+            if (command.hit) {
+                command.hit = false;
+
+                wsConnection.send(command.stopCommand);
+            }
+        }
+    }, false);
 })();
